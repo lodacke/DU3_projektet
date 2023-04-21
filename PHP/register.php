c7294f0cc1995525fe178bab33f9053d3621c8e9
lodacke <lo.dacke@mail.com>
2023-04-21 15:59:32 +0200

    fixed the json file and enabled from js and PHP to add to the file. Also added a text-css index (cause it made my eyes hurt)

--- PHP/register.php
+++ PHP/register.php
@@ -21,29 +21,49 @@ if($_SERVER["REQUEST_METHOD"] === "POST"){
     $password = $dataREQUEST["password"];
     $age = $dataREQUEST["age"];
     $gender = $dataREQUEST["gender"];
-    //$image = $dataREQUEST["image"];
+    //$image = $dataREQUEST["image"];  
+    $userInfo = $dataREQUEST["interests"][0]["userInfo"];
+    $contact = $dataREQUEST["interests"][0]["contact"];
+    $genderOf = $dataREQUEST["preference"][0]["genderOf"];   
+    $ageOf = $dataREQUEST["preference"][0]["ageOf"];
+                                 
 
-   // for($i = 0; $i < count($users); $i++){
-   //  if($username == $users[$i]["username"]){        
-   //      header("Content-type: application/json");
-   //      http_response_code(409);
-   //      $message = "The username is already taken";
-   //      json_encode($message);
-   //      echo $message;
-   //      exit();  
-   //  }  
-   //}  
-             
-     if(!$username == "" && $password == "" && $age == "" && $gender == "" && $image == ""){
-     
+    for($i = 0; $i < count($users); $i++){
+       if($username == $users[$i]["username"]){        
+        header("Content-type: application/json");
+        http_response_code(409);
+        $message = "The username is already taken";
+        echo (json_encode($message));
+        exit();  
+     }  
+   }  
+
+   if($age < 18){
+        header("Content-type: application/json");
+        http_response_code(409);
+        $message = "You need to be over 18 to use this app";
+        echo (json_encode($message));
+        exit();
+   }
+
+    if(!($username == "" && $password == "" && $gender == "none" && $userInfo == "" && $contact == "" && $age == null)){
+        
         $newUser = [
           "username" => $username,
           "password" => $password,
           "age" => $age,
           "gender" => $gender,
-          "image" => $image,
+         // "image" => $image,
+          "interests" => [
+            "userInfo" => $userInfo,
+            "contact" => $contact,
+          ],
+          "preference" => [
+            "genderOf" => $genderOf,
+            "ageOf" => $ageOf,
+          ],
           ];
-          array_push($users, $newUser);
+          $users[] = $newUser;
 
         $data = json_encode($users, JSON_PRETTY_PRINT);
         file_put_contents($fileName, $data);
@@ -51,8 +71,7 @@ if($_SERVER["REQUEST_METHOD"] === "POST"){
          header("Content-type: application/json");
          http_response_code(200);
          $message = "A new user have been added!";
-         json_encode($message);
-         echo $message;
+         echo (json_encode($message));
          exit();  
 
 
@@ -60,11 +79,16 @@ if($_SERVER["REQUEST_METHOD"] === "POST"){
             header("Content-type: application/json");
             http_response_code(401);
             $message = "You need to fill in all the fields before you proceed";
-            json_encode($message);
-            echo $message;
+            echo (json_encode($message));
             exit();  
-         }
+         }            
+    } else {
+        http_response_code(405);
+        $message = "Wrong HTTP-method";
+        echo (json_encode($message));
+        exit(); 
+        
+    }
 
-};
 
 ?>
--- register.js
+++ register.js
@@ -5,58 +5,143 @@ let mainDom = document.querySelector("main");
 let request = "PHP/register.php";
 
 mainDom.innerHTML = `
-<lable for "name"> Username </lable>
+<lable for "name"> Username: </lable>
 <input type="username" name="username"> 
 
-<lable for "password"> Password </lable>
+<lable for "password"> Password: </lable>
 <input type="password" name="password"> 
 
-<lable for "age"> Age</lable>
-<input name="age">
+<lable for "age"> Age: </lable>
+<input type="number" name="age">
 
-<lable for "image"> Image </lable>
+<lable for "image"> Profile photo: </lable>
 <input type="file" name="image">
 
-<lable for "gender"> Gender </lable>
-<select list="browsers" name="gender"> 
+<lable for "gender"> Gender: </lable>
+<select name="gender"> 
+<option value="none">Choose an option </option>
 <option value="female"> Female </option>
 <option value="male"> Male </option>
 <option value="None">Dont want to say</option>
 </select>
+<button id="pageOne">Next page</button>`;
 
-
-<button>Skicka in</button>`;
 let username_dom = mainDom.querySelector("input[name='username']");
 let password_dom = mainDom.querySelector("input[name='password']");
 let age_dom = mainDom.querySelector("input[name='age']");
-let image_dom = mainDom.querySelector("input[name='image']");
+//let image_dom = mainDom.querySelector("input[name='image']");
 let gender_dom = mainDom.querySelector("select[name='gender']");
 
-mainDom.querySelector("button").addEventListener("click", addUser);
+mainDom.querySelector("#pageOne").addEventListener("click", e => { 
 
-async function addUser(){
+    if(username_dom.value != "" && password_dom.value != "" && age_dom != null && gender_dom != "none"){
 
-    console.log(age_dom.value);
+    let userData = { 
+        username: username_dom.value,
+        password: password_dom.value,
+        age: age_dom.value,
+        gender: gender_dom.value,
+        interests: [],
+        preference: [],
+        // image: image_dom.value,
+    };
+
+    mainDom.innerHTML =``;
+    pageTwo(userData);
+
+    function pageTwo(userData){
+        mainDom.innerHTML = `
+        <lable for "userInfo">Tell people about yourself:</lable>
+        <input name="userInfo">
+
+        <lable for "contact">How do you want people to contact you?</lable>
+        <input name="contact" placeholder="email or phonenumber">
+
+        <button id=pageTwo>Next Page</button>
+        `;
+
+        let userInfo = mainDom.querySelector("input[name='userInfo']");
+        let contact = mainDom.querySelector("input[name='contact']");
+
+        mainDom.querySelector("#pageTwo").addEventListener("click", e => {
+        
+            if(userInfo.value != "" && contact.value != ""){
+            
+                let preference = {
+                     userInfo: userInfo.value,
+                     contact: contact.value
+                    };
+
+                userData.interests.push(preference);
+
+                console.log(userData);
+                mainDom.innerHTML =``;
+                pageThree(userData);
+
+                function pageThree(userData){
+                    mainDom.innerHTML =`
+                    <lable for "genderOf"> I'm intrested in: </lable>
+                    <select name="genderOf">
+                    <option value="none">Choose an option </option>
+                    <option value="female"> Female </option>
+                    <option value="male"> Male </option>
+                    <option value="None">Dont want to say</option>
+                    </select>
+
+                    <lable for "ageOf"> What age: </lable>
+                    <input type="number" name="ageOf">    
+
+                    <button id="submitUser">Start dating!</button>           
+                    `;
+
+                    let genderOf = mainDom.querySelector("select[name='genderOf']");
+                    let ageOf = mainDom.querySelector("input[name='ageOf']");
+
+                    mainDom.querySelector("#submitUser").addEventListener("click", e => { 
+                       if(genderOf.value != "none" && ageOf.value != null){
+
+                            let preference = {
+                                genderOf: genderOf.value,
+                                ageOf: ageOf.value
+                            };
+                            userData.preference.push(preference);
+                            addUser(userData);
+                            //mainDom.innerHTML = ``;
+                            }
+                        })
+                    }
+                }           
+            })
+
+        } //else { 
+    // Här måste det läggas till antingen en fetch så att man kan nå felmeddelandena (problemet är att userData är i fel scope)
+    // Eller läggas till felkoder manuellt för de olika casen.
+    } //else addUser() kansk??
+});
+
+async function addUser(userData){
 
     let requestPOST = await fetch( new Request(request, {
     method: "POST",
     headers: {"Content-type":"application/json; charset=UTF-8"},
-    body: JSON.stringify({
-         action: "register", 
-         username: username_dom.value,
-         password: password_dom.value,
-         age: age_dom.value,
-         gender: gender_dom.value,
-         image: image_dom.value,
-       
-         })
-}));  
+    body: JSON.stringify(userData)
+    }));  
 
-if(requestPOST.status === 200){
-    console.log("added a user");
-} else {
-    console.log(requestPOST);
-}
+    let JSONresponse = await requestPOST.json();
+
+    let p_dom = document.createElement("p");
+    document.querySelector("main").append(p_dom);
+
+    if(requestPOST.status === 200){
+        p_dom.textContent = ""; //Försök lägga p_dom i en egen funktion. 
+        p_dom.textContent = `${JSONresponse}`;
+
+       // mainDom.innerHTML = ``;
+
+    } else {
+        p_dom.textContent = "";
+        p_dom.textContent = `errorcode: ${requestPOST.statusText}, ${JSONresponse}`;     
+    }
 
 }
 
